e57e29230022decf11a461be49aa48090122e6dd
Thomas Roussel <thomas.roussel@ik.me>
2022-02-17 19:00:51 +0100

    all fetch ok

--- script.js
+++ script.js
@@ -1,41 +1,84 @@
-getBestMovie()
+
+
+fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&page_size=7&page=1")
     .then(response => {
-        console.log("Best Movie : OK");
-        console.log(response);
+        return response.json();
+    })
+    .then(json => {
+        return document.getElementById("best-image").src = json.results[0].image_url;
     })
     .catch(error => {
-        console.log("Best Movie : Error");
         console.error(error);
-    });
+    })
 
-getMostRated()
+fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&page_size=7&page=1")
     .then(response => {
-        console.log("Most Rated : OK");
-        console.log(response);
+        return response.json();
+    })
+    .then(json => {
+        let filmContainer = document.getElementById("most-rated-images");
+        let filmList = json.results;
+        filmList.forEach(function(film) {
+            let imageURL = film.image_url;
+            let filmImage = document.createElement("img");
+            filmImage.setAttribute("src", [imageURL]);
+            filmContainer.appendChild(filmImage);
+        })
     })
     .catch(error => {
-        console.log("Most Rated : Error");
         console.error(error);
-    });
+    })
 
-async function getBestMovie() {
-    let response = await fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&page_size=7&page=1");
-    response = await response.json();
-    let filmImage = await response.results[0].image_url;
-    return document.getElementById("best-image").src = filmImage;
-}
+fetch("http://localhost:8000/api/v1/titles/?genre=fantasy&page_size=7&page=1")
+    .then(response => {
+        return response.json();
+    })
+    .then(json => {
+        let filmContainer = document.getElementById("fantasy-images");
+        let filmList = json.results;
+        filmList.forEach(function(film) {
+            let imageURL = film.image_url;
+            let filmImage = document.createElement("img");
+            filmImage.setAttribute("src", [imageURL]);
+            filmContainer.appendChild(filmImage);
+        })
+    })
+    .catch(error => {
+        console.error(error);
+    })
 
-async function getMostRated() {
-    let response = await fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&page_size=7&page=1");
-    response = await response.json();
-    console.log(response);
-    let filmList = response.results;
-    let index = 0;
-    for(let film in filmList) {
-        let filmImage = await document.createElement("img");
-        let imageIndex = `image${index}`;
-        index += 1
-        await filmImage.setAttribute("id", imageIndex);
-        document.getElementById(imageIndex).src = film.image_url;
-    }
-}
+fetch("http://localhost:8000/api/v1/titles/?genre=sci-fi&page_size=7&page=1")
+    .then(response => {
+        return response.json();
+    })
+    .then(json => {
+        let filmContainer = document.getElementById("sci-fi-images");
+        let filmList = json.results;
+        filmList.forEach(function(film) {
+            let imageURL = film.image_url;
+            let filmImage = document.createElement("img");
+            filmImage.setAttribute("src", [imageURL]);
+            filmContainer.appendChild(filmImage);
+        })
+    })
+    .catch(error => {
+        console.error(error);
+    })
+
+fetch("http://localhost:8000/api/v1/titles/?genre=animation&page_size=7&page=1")
+    .then(response => {
+        return response.json();
+    })
+    .then(json => {
+        let filmContainer = document.getElementById("animation-images");
+        let filmList = json.results;
+        filmList.forEach(function(film) {
+            let imageURL = film.image_url;
+            let filmImage = document.createElement("img");
+            filmImage.setAttribute("src", [imageURL]);
+            filmContainer.appendChild(filmImage);
+        })
+    })
+    .catch(error => {
+        console.error(error);
+    })

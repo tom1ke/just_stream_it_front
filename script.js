9857f4bca0988e2fbab2e97ff1dc118dc6c8dba8
Thomas Roussel <thomas.roussel@ik.me>
2022-02-18 12:45:01 +0100

    func to get categories

--- script.js
+++ script.js
@@ -29,56 +29,26 @@ fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&page_size=7&page
         console.error(error);
     })
 
-fetch("http://localhost:8000/api/v1/titles/?genre=fantasy&page_size=7&page=1")
-    .then(response => {
-        return response.json();
-    })
-    .then(json => {
-        let filmContainer = document.getElementById("fantasy-images");
-        let filmList = json.results;
-        filmList.forEach(function(film) {
-            let imageURL = film.image_url;
-            let filmImage = document.createElement("img");
-            filmImage.setAttribute("src", [imageURL]);
-            filmContainer.appendChild(filmImage);
+function getCategory(category) {
+    fetch(`http://localhost:8000/api/v1/titles/?genre=${category}&page_size=7&page=1`)
+        .then(response => {
+            return response.json();
         })
-    })
-    .catch(error => {
-        console.error(error);
-    })
+        .then(json => {
+            let filmContainer = document.getElementById(`${category}-images`);
+            let filmList = json.results;
+            filmList.forEach(function(film) {
+                let imageURL = film.image_url;
+                let filmImage = document.createElement("img");
+                filmImage.setAttribute("src", [imageURL]);
+                filmContainer.appendChild(filmImage);
+            })
+        })
+        .catch(error => {
+            console.error(error);
+        })
+}
 
-fetch("http://localhost:8000/api/v1/titles/?genre=sci-fi&page_size=7&page=1")
-    .then(response => {
-        return response.json();
-    })
-    .then(json => {
-        let filmContainer = document.getElementById("sci-fi-images");
-        let filmList = json.results;
-        filmList.forEach(function(film) {
-            let imageURL = film.image_url;
-            let filmImage = document.createElement("img");
-            filmImage.setAttribute("src", [imageURL]);
-            filmContainer.appendChild(filmImage);
-        })
-    })
-    .catch(error => {
-        console.error(error);
-    })
-
-fetch("http://localhost:8000/api/v1/titles/?genre=animation&page_size=7&page=1")
-    .then(response => {
-        return response.json();
-    })
-    .then(json => {
-        let filmContainer = document.getElementById("animation-images");
-        let filmList = json.results;
-        filmList.forEach(function(film) {
-            let imageURL = film.image_url;
-            let filmImage = document.createElement("img");
-            filmImage.setAttribute("src", [imageURL]);
-            filmContainer.appendChild(filmImage);
-        })
-    })
-    .catch(error => {
-        console.error(error);
-    })
+getCategory("fantasy")
+getCategory("sci-fi")
+getCategory("animation")
